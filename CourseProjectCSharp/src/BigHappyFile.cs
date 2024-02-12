e16034dad1c1abf22180d709fe66f48ca715679f
JonatanRasmussen <115923496+JonatanRasmussen@users.noreply.github.com>
2024-02-12 09:36:04 +0100

    Init commit

---
+++ CourseProjectCSharp/src/BigHappyFile.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+
+namespace GlobalNameSpace;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+    }
+}
---
+++ CourseProjectTests/tests/Tests.cs
@@ -0,0 +1,19 @@
+namespace TestingNameSpace;
+
+using GlobalNameSpace;
+using System;
+using Xunit;
+
+public class InfoParsingTests
+{
+    [Fact]
+    public void InfoParsingTest1()
+    {
+        Assert.Equal(1, 1);
+    }
+    [Fact]
+    public void InfoParsingTest2()
+    {
+        Assert.Equal(2, 2);
+    }
+}
